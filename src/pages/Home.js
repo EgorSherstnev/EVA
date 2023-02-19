8531e10586de6844df3362fe4d7f977455842dfc
EgorSherstnev <svarog91@list.ru>
2023-02-19 18:21:19 +0300

    added HeroHome

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../partials/Header";
+import HeroHome from "../partials/HeroHome";
 
 function Home() {
     return (
-        <div >
+        <div className="flex flex-col min-h-screen overflow-hidden">
+            {/*  Site header */}
            <Header />
+
+           {/*  Page content */}
+            <main className="grow">
+                {/*  Page sections */}
+                <HeroHome />
+            </main>
         </div>
     );
 }
