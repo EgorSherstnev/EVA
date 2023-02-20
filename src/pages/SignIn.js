77f1202ffd611dd197729c2336a2e848ea77ea04
Lexandrina90 <lexandrina90@gmail.com>
2023-02-20 23:37:13 +0300

    Sign_in not ready!

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Header from '../partials/Header';
+import PageIllustration from '../partials/PageIllustration';
+import Banner from '../partials/Banner';
+
 function SignIn() {
     return (
-        <div>
-            SignIn page
+        <div className="flex flex-col min-h-screen overflow-hidden">
+           {/* Site header */}
+           <Header />
+
+           {/* Page content */}
+           <main className="grow">
+
+                {/* Page illustration */}
+                <div className="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+                    <PageIllustration />
+                </div>
+
+           </main>
         </div>
     );
 }
