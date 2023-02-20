0da6bb280fef0613d2940bafadad96240ddc2737
Lexandrina90 <lexandrina90@gmail.com>
2023-02-20 22:10:31 +0300

    Hamburger_btn done

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -108,9 +108,10 @@ return (
                     <button ref={trigger} className={`hamburger ${mobileNavOpen && 'active'}`} aria-controls="mobile-nav" aria-expanded={mobileNavOpen} onClick={() => setMobileNavOpen(!mobileNavOpen)}>
                         <span className="sr-only">Menu</span>
                         <svg className="w-6 h-6 fill-current text-gray-300 transition duration-150 ease-in-out" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-
+                            <rect y="4" width="24" height="2" rx="1" />
+                            <rect y="11" width="24" height="2" rx="1" />
+                            <rect y="18" width="24" height="2" rx="1" />
                         </svg>
-
                     </button>
 
 
