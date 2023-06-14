5dd5ebed1dad29592822bdf6e3fd799362675e62
EgorSherstnev <svarog91@list.ru>
2023-06-14 22:29:24 +0300

    update server/userRouter(logout,activate,refresh)

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,5 +6,10 @@ const authMiddleware = require('../middleware/authMiddleware')
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', authMiddleware, userController.check)
+router.post('/logout')
+router.get('/activate/:link')
+router.get('/refresh')
+
+router.get('/users')
 
 module.exports = router;
